9c03eb587345b5ff0730ad0f2a6a4202d8580246
shilinni <shellyni1996@gmail.com>
2019-04-22 19:19:55 -0400

    ...

--- sic/local_model.py
+++ sic/local_model.py
@@ -5,6 +5,9 @@ from keras.layers import Conv2D, Flatten, MaxPooling2D
 from keras.layers.core import Dense, Dropout, Activation
 from keras import optimizers
 from keras.utils.np_utils import to_categorical
+from tensorflow.train import GradientDescentOptimizer
+from privacy.dp_query.gaussian_query import GaussianAverageQuery
+from privacy.optimizers.dp_optimizer import DPGradientDescentOptimizer
 
 from sklearn import svm
 
@@ -24,8 +27,8 @@ class LocalModel(object):
         opts['noise_multiplier'] = 1.1
         opts['l2_norm_clip'] = 1.0
         opts['batch_size'] = 250
-        opts['epochs'] = 300
-        opts['microbatches'] = 1
+        opts['epochs'] = 60
+        opts['microbatches'] = 250
         opts['model_dir'] = None
         return opts
 
@@ -69,13 +72,21 @@ class LocalCNNModel(LocalModel):
     def __init__(self, input_dim=28, output_dim=10,input_shape=(28,28,1),flags=None,optimizer='adam'):
         self.input_dim = input_dim
         self.output_dim = output_dim
-        model = Sequential()
-        model.add(Conv2D(input_dim, kernel_size=(3,3), input_shape=input_shape))
-        model.add(MaxPooling2D(pool_size=(2, 2)))
-        model.add(Flatten()) # Flattening the 2D arrays for fully connected layers
-        model.add(Dense(128, activation=tf.nn.relu))
-        model.add(Dropout(0.2))
-        model.add(Dense(output_dim,activation=tf.nn.softmax))
+        model = tf.keras.Sequential([
+        tf.keras.layers.Conv2D(16, 8,
+                             strides=2,
+                             padding='same',
+                             activation='relu',
+                             input_shape=input_shape),
+        tf.keras.layers.MaxPool2D(2, 1),
+        tf.keras.layers.Conv2D(32, 4,
+                             strides=2,
+                             padding='valid',
+                             activation='relu'),
+        tf.keras.layers.MaxPool2D(2, 1),
+        tf.keras.layers.Flatten(),
+        tf.keras.layers.Dense(32, activation='relu'),
+        tf.keras.layers.Dense(output_dim)])
         
         if flags is None:
             flags = self.default_flags()
@@ -90,16 +101,20 @@ class LocalCNNModel(LocalModel):
                 flags['microbatches'],
                 learning_rate=flags['learning_rate'],
                 unroll_microbatches=True)
-        
-        model.compile(optimizer=optimizer, 
-              loss='categorical_crossentropy', 
-              metrics=['accuracy'])
+            loss = tf.keras.losses.CategoricalCrossentropy(from_logits=True, reduction=tf.losses.Reduction.NONE)
+        else:
+            optimizer = GradientDescentOptimizer(learning_rate=flags['learning_rate'])
+            loss = tf.keras.losses.CategoricalCrossentropy(from_logits=True)
+        model.compile(optimizer=optimizer, loss=loss, metrics=['accuracy'])
         
         self.model=model
         
     def fit(self, X, y, epochs=10):
         y_categorical = to_categorical(y, num_classes=self.output_dim)
-        self.model.fit(x=X,y=y_categorical, epochs=epochs)
+        self.model.fit(X, y,
+            epochs=epochs,
+            batch_size=self.flags['batch_size'])
+        #self.model.fit(x=X,y=y_categorical, epochs=epochs)
         
     def predict_classes(self, X):
         return self.model.predict_classes(X)    
