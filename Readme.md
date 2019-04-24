51df019b656859f7ca03ebc3a9ae5fe6d21b4ed8
Brian Goodchild <brianjosephg@gmail.com>
2019-04-24 16:38:37 -0400

    added CIFAR meta-model and readme

--- Readme.md
+++ Readme.md
@@ -11,6 +11,84 @@ $ cd sharing-is-caring
 $ pip install .
 ```
 
+## Introduction
+Sharing Is Caring(SIC) is a framework for collaborative machine learning. The SIC
+protocol allows a set of peers, or *local experts* to collaborate on classification
+problems by privately training a *meta-model.* The protocol works as follows:
+
+1. Each peer, *e* trains a local model *M_e* (*M_e* is assumed to have been trained 
+with a privacy mechanism chosen by *e*).
+2. Each peer broadcasts *M_e* to all other participating peers.
+3. Peers collaboratively train a *meta-model*, *MM*, that maps inputs to local models as follows:
+..a. A centralized *MM*, along with its loss function, is initialized and broadcast to all peers.
+..b. For rounds *t* := 1,2,...,*T*:
+....1) Each peer calculates the gradient *g_et* of *MM_t-1* using local data only available to *e*. Here, noise is added to ensure differential privacy of the meta-model training data.
+....2) Each peer broadcasts *g_pt* to all other peers.
+....3) Each per updates *MM_t-1* to *MM_t* by averaging the gradients of all participating peers.
+
+We assume that all peers participate honestly in the protocol except that some peers may be interested in learning about the data that other peers have trained on.
+
+## Prototype
+Our prototype is a simple meta-model located in `sic/meta_model.py`. It takes as arguments to the constructor a list of peers' models, and, the peers' local training data. Note that 
+in a real implementation, each peer would have its own version of the meta-model and would broadcast gradient updates to other peers, ensuring that no one meta-model has access to all
+peers' local training data.
+
+If we have a set of pre-trained local models saved in the variable `models`, we can create new meta-model as follows:
+```python
+...
+mm = MetaModel(input_shape, models, flags)
+```
+
+Meta-model training occurs by calling the `compute_gradient` or `update_gradient` functions:
+```python
+# x_local_train and y_local_train are training data and labels local to each
+# peer. In practice, these would be fully isolated.
+epocs = 10
+for i in range(epochs):
+    for j in range(npeers):
+        mm.compute_gradient(x_local_train[j], y_local_train[j], update=True)
+```
+
+Alternatively:
+```python
+for i in range(epochs):
+    mm.update_gradient([
+        mm.compute_gradient(x_local_train[j], y_local_train[j], update=False)
+        for j in range(npeers)])
+```
+
+The `flags` parameter is used to control the addition of DP noise
+```python
+flags = dict()
+flags['dpsgd'] = True               # add noise on/off
+flags['learning_rate'] = 0.15       # size of each gradient step
+flags['noise_multiplier'] = 1.01    # amount of noise added (propto l2_norm_clip)
+flags['l2_norm_clip'] = 1.0         # the maximimum size of a gradient update
+flags['batch_size'] = 250           # unused
+flags['epochs'] = 3                 # unused
+flags['microbatches'] = 1           # unused
+flags['model_dir'] = None
+```
+
+Although the meta-model is centralized, in practice this would not be the case. Each peer is supposed to see only the gradient update at step *t*, and not the full local training data. 
+It would look like this:
+
+```python
+for i in range(epochs):
+    wait_for_peers(i) # does not wait when i==0
+
+    updates = [mm.compute_gradient(x_local_train, y_local_train)]
+    updates += get_gradient_updates_from_peers(...)
+    mm.update_gradient(updates)
+
+    notify_peers_round(i+1)
+```
+
+As long as the gradient descent algorithm being used is deterministic, all peers will collaboratively make steps towards an optimal solution.
+
+## Red Team Instructions
+For our red team, we will provide a fully-trained meta-model, along with the global training data used by all experts, and the history of all gradient steps calculated throughout the optimization process. Your goal should be to determine if particular training data was used by any of the participating peers to train the meta-model.
+
 ## Authors
 * Geelon So 
 * Shilin Ni
--- sic/meta_model.py
+++ sic/meta_model.py
@@ -1,17 +1,20 @@
 import numpy as np
 from keras.models import Sequential
 from keras.layers.core import Dense, Activation
-from keras import optimizers
+import keras.optimizers
 import tensorflow as tf
 from tensorflow.train import GradientDescentOptimizer
 from privacy.dp_query.gaussian_query import GaussianAverageQuery
-from privacy.optimizers.dp_optimizer import DPGradientDescentOptimizer
+from privacy.optimizers.dp_optimizer import DPGradientDescentOptimizer, make_optimizer_class
+
+from keras.layers import Dropout, Flatten
+from keras.layers import Conv2D, MaxPooling2D
 
 # META MODEL
 class MetaModel:
     def __init__(self,  input_shape, models, X=None, y=None,flags=None):
         """
-        Implements meta-model
+        Implements a linear meta-model
 
         Args:
         X: train data input
@@ -25,10 +28,9 @@ class MetaModel:
         self.ground_truth = y
         self.models = models
 
-
         # compute expert_correctness matrix
         if X is not None:
-          self.Y = self.zero_one_loss(X, y, models)
+            self.Y = self.zero_one_loss(X, y, models)
 
         # initialize meta-model
         self.model = model = Sequential([
@@ -36,43 +38,38 @@ class MetaModel:
             Activation('relu'),
             Dense(num_peers),
             Activation('softmax'),
-        ])
+            ])
 
         # set flags
         if flags is None:
-            flags = self.default_flags()
+            flags = MetaModel.default_flags()
         self.flags = flags
-        
-        
+
         # optimizer (see code from mnist example in tensoflow/privacy)
         if flags['dpsgd']:
             dp_average_query = GaussianAverageQuery(
-                flags['l2_norm_clip'],
-                flags['l2_norm_clip'] * flags['noise_multiplier'],
-                flags['microbatches'])
+                    flags['l2_norm_clip'],
+                    flags['l2_norm_clip'] * flags['noise_multiplier'],
+                    flags['microbatches'])
             optimizer = DPGradientDescentOptimizer(
-                dp_average_query,
-                flags['microbatches'],
-                learning_rate=flags['learning_rate'],
-                unroll_microbatches=True)
+                    dp_average_query,
+                    flags['microbatches'],
+                    learning_rate=flags['learning_rate'],
+                    unroll_microbatches=True)
         else:
             optimizer = GradientDescentOptimizer(learning_rate=flags['learning_rate'])
 
-            
-
-
         model.compile(optimizer=optimizer, loss='mean_squared_error')
         self.model = model
-
 
     def predict_classes(self, X, batch_size=None):
         """Given a list of input data, return the output prediction.
         """
         which_expert = self.get_experts(X)
         predictions = np.transpose([model.predict_classes(X)
-                                    for model in self.models])
+            for model in self.models])
         expert_prediction = [pair[1][pair[0]]
-                             for pair in zip(which_expert, predictions)]
+                for pair in zip(which_expert, predictions)]
         return np.array(expert_prediction)
 
     def accuracy(self, X_test, y_test):
@@ -82,10 +79,10 @@ class MetaModel:
         """Run the fit function on model.
         """
         if x is not None:
-          py=self.zero_one_loss(x,y,self.models)
-          self.model.fit(x, py,batch_size=1)
+            py=self.zero_one_loss(x,y,self.models)
+            self.model.fit(x, py,batch_size=1)
         else:
-          self.model.fit(self.X, self.Y,batch_size=1)
+            self.model.fit(self.X, self.Y,batch_size=1)
 
     def get_experts(self, X):
         """Given a list of input data, return a list of experts for each data.
@@ -101,7 +98,7 @@ class MetaModel:
         """Sets the weights for each layer in the model
         """
         self.model.set_weights(weights)
-            
+
     def compute_gradient(self, X=None, Y=None, update=True):
         """
         Computes gradients, possibly updating the meta-model
@@ -115,17 +112,17 @@ class MetaModel:
                 if False, compute_gradient will have no side-effect on MetaModel
         """
         if X is None:
-          X = self.X
-          Y = self.Y
+            X = self.X
+            Y = self.Y
         else:
-          Y=self.zero_one_loss(X,Y,self.models)
+            Y=self.zero_one_loss(X,Y,self.models)
 
         saved_weights = self.get_weights()
         self.model.train_on_batch(X, Y)
         new_weights = self.get_weights()
-        
+
         gradient = [new - old for (new, old) in zip(new_weights, saved_weights)]
-        
+
         if not update:
             self.set_weights(saved_weights)
 
@@ -138,15 +135,15 @@ class MetaModel:
         gradient = [0 for _ in weights]
         num_gradients = len(gradients)
         for steps in gradients:
-            gradient = [weight + (step / num_gradients)  
-                        for (weight, step) in zip(gradient, steps)]
-        
-        updates = [weight + step for (weight, step) in zip(weights, gradient)]
+            gradient = [weight + (step / num_gradients)
+                    for (weight, step) in zip(gradient, steps)]
+
+            updates = [weight + step for (weight, step) in zip(weights, gradient)]
         self.model.set_weights(updates)
 
 
     @staticmethod
-    def zero_one_loss(X, y,models):
+    def zero_one_loss(X, y, models):
         """Computes expert-instance correctness matrix.
         Args
         X: training instances
@@ -184,3 +181,72 @@ class MetaModel:
         opts['microbatches'] = 1
         opts['model_dir'] = None
         return opts
+
+class MetaModelCIFAR(MetaModel):
+
+    def __init__(self, input_shape, models, X=None, y=None, flags=None):
+        """
+        Implements a CNN meta-model for use in image classification.
+
+        Args:
+        X: train data input
+        y: train data labels
+        models: list of models
+        flags: tensorflow flags
+        """
+        self.num_peers = num_peers = len(models)
+        self.X = X
+        self.ground_truth = y
+        self.models = models
+
+        # compute expert_correctness matrix
+        if X is not None:
+            self.Y = self.zero_one_loss(X, y, models)
+
+        model = Sequential()
+        model.add(Conv2D(32, (3, 3), padding='same',
+            input_shape=input_shape))
+        model.add(Activation('relu'))
+        model.add(Conv2D(32, (3, 3)))
+        model.add(Activation('relu'))
+        model.add(MaxPooling2D(pool_size=(2, 2)))
+        model.add(Dropout(0.25))
+
+        model.add(Conv2D(64, (3, 3), padding='same'))
+        model.add(Activation('relu'))
+        model.add(Conv2D(64, (3, 3)))
+        model.add(Activation('relu'))
+        model.add(MaxPooling2D(pool_size=(2, 2)))
+        model.add(Dropout(0.25))
+
+        model.add(Flatten())
+        model.add(Dense(512))
+        model.add(Activation('relu'))
+        model.add(Dropout(0.5))
+        model.add(Dense(num_peers))
+        model.add(Activation('softmax'))
+
+
+        # set flags
+        if flags is None:
+            flags = MetaModel.default_flags()
+        self.flags = flags
+
+        # optimizer (see code from mnist example in tensoflow/privacy)
+        if flags['dpsgd']:
+            dp_average_query = GaussianAverageQuery(
+                    flags['l2_norm_clip'],
+                    flags['l2_norm_clip'] * flags['noise_multiplier'],
+                    flags['microbatches'])
+
+            optimizer = DPGradientDescentOptimizer(
+                    dp_average_query,
+                    flags['microbatches'],
+                    learning_rate=flags['learning_rate'],
+                    unroll_microbatches=True)
+        else:
+            optimizer = GradientDescentOptimizer(learning_rate=flags['learning_rate'])
+
+        model.compile(optimizer=optimizer, loss='mean_squared_error')
+
+        self.model = model
